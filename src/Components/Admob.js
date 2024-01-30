a9243b75d814417282f64b293b4cd8329ae9bf7d
Jayanth <jayanthbharadwajm@gmail.com>
2024-01-30 11:22:13 +0530

    Minor change to Admob component

--- src/Components/Admob.js
+++ src/Components/Admob.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+
+let AD_UNITS = [
+  "ca-app-pub-0714649342045057/8222174259",
+  "ca-app-pub-0714649342045057/5615643477",
+  "ca-app-pub-0714649342045057/1676398465",
+  "ca-app-pub-0714649342045057/6338246941",
+  "ca-app-pub-0714649342045057~1333332271",
+];
+function generateId() {
+  try {
+    let rN = Math.floor(Math.random() * AD_UNITS.length);
+    return AD_UNITS[rN];
+  } catch (error) {
+    return AD_UNITS[0];
+  }
+}
 const Admob = () => {
+  let unitId = GenerateId();
   return (
-    <View style={{height: 50, backgroundColor: '#fff'}}>
-      <Text style={{textAlign: 'center'}}>Placeholder for Admob</Text>
+    <View style={{ height: 50, backgroundColor: "#fff" }}>
+      <Text style={{ textAlign: "center" }}>unitId</Text>
     </View>
   );
 };
