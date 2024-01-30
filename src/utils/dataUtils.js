9e17e0a55d015b942e295563c08bba3f4e945d76
Jayanth <jayanthbharadwajm@gmail.com>
2024-01-30 11:30:18 +0530

    new function added

---
+++ src/utils/dataUtils.js
@@ -0,0 +1,11 @@
+const THRESHOLDS = {
+  HOME: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+  LIST: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+  READER: 2 * 24 * 60 * 60 * 1000, // 2 days in milliseconds
+  SETTING: 30 * 24 * 60 * 60 * 1000, // 30 days in milliseconds
+};
+
+const getDataItem = (KEYNAME, URL, SCREEN_TYPE) => {
+  try {
+  } catch (error) {}
+};
