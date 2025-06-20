1962b933f759b012f9af1ed245df920455ea835d
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-06-20 23:37:02 +0530

    refactor: reorganize imports in headerLeft component for better readability

--- src/components/headerLeft.js
+++ src/components/headerLeft.js
@@ -1,18 +1,24 @@
-import PropTypes from 'prop-types';
 import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
 
-import CustomIcon from './customIcon';
-import { ThemeContext } from '../contexts/themeContext';
 import { COLOR_SCHEME } from '../styles/styles';
+import CustomIcon from './customIcon';
+import PropTypes from 'prop-types';
+import { StyleSheet } from 'react-native';
+import { ThemeContext } from '../contexts/themeContext';
+
 const CustomHeaderLeft = ({ navigation }) => {
   const { darkmode } = useContext(ThemeContext);
+
   const styles = StyleSheet.create({
     headerIcon: {
       marginLeft: 15,
       color: COLOR_SCHEME[darkmode ? 'DARK' : 'LIGHT'].headertext,
     },
   });
+
+  if (!navigation.canGoBack()) {
+    return null;
+  }
 
   const renderButton = navigation.goBack && (
     <CustomIcon
