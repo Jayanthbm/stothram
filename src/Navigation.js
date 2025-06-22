a7aa63193037ac12e13e634ac0903577fa25e2d3
Jayanthbharadwaj M <jayanthbharadwajm@gmail.com>
2025-06-22 12:15:22 +0530

    refactor: reorganize imports and enhance navigation options in Navigation component

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,41 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { COLOR_SCHEME } from './styles/styles';
 import HomeScreen from './screens/HomeScreen';
 import ListScreen from './screens/ListScreen';
+import { NavigationContainer } from '@react-navigation/native';
 import ReaderScreen from './screens/ReaderScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import { ThemeContext } from './contexts/themeContext';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 const Navigation = () => {
+  const { darkmode } = useContext(ThemeContext);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        headerMode="screen"
+        initialRouteName="Home"
+        screenOptions={{
+          headerTitleAlign: 'left',
+          statusBarTranslucent: false,
+          headerStyle: {
+            backgroundColor: darkmode
+              ? COLOR_SCHEME.DARK.headerBackground
+              : COLOR_SCHEME.LIGHT.headerBackground,
+          },
+          headerTintColor: darkmode
+            ? COLOR_SCHEME.DARK.headertext
+            : COLOR_SCHEME.LIGHT.headertext,
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            fontSize: 16,
+            fontFamily: 'NotoSans',
+          },
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="List" component={ListScreen} />
         <Stack.Screen name="Reader" component={ReaderScreen} />
